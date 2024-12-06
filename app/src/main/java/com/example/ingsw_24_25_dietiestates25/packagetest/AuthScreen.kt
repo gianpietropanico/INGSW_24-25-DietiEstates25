cec637fbfaaa1ddf12c26f553564ef595bf42008
ADOLFO <TORCY.ADO@GMAIL.COM>
2024-12-06 16:08:46 +0100

    classi di test per hiltedview e navigator NON MODIFICARE O CANCELLARE

---
+++ app/src/main/java/com/example/ingsw_24_25_dietiestates25/packagetest/AuthScreen.kt
@@ -0,0 +1,144 @@
+package com.example.ingsw_24_25_dietiestates25.packagetest
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.ingsw_24_25_dietiestates25.data.auth.AuthResult
+import com.plcoding.jwtauthktorandroid.ui.destinations.AuthScreenDestination
+import com.plcoding.jwtauthktorandroid.ui.destinations.SecretScreenDestination
+
+import kotlinx.coroutines.flow.collect
+/*TODO CAPIRE COME SI NAVIGA FRA GLI SCHERMI E COME FUNZIONA HILT MODEL
+*  LUI UTILIZZA UN NAVIGATOR DI UN TIPO SU GITHUB CAZZ NE SO
+* INOLTRE C'è UNA VIEW MODEL CHE SI CHIAMA HILTEDVIEW MODEL CHE NON HO CAPITO BENE COSA'è
+* FACCIO COMMIT E POI SASERA CERCO DI VEDE DI RISOLVE*/
+
+@Composable
+@Destination(start = true)
+fun AuthScreen(
+    navigator: DestinationsNavigator,
+    viewModel: MainViewModel2 = hiltViewModel()
+) {
+    val state = viewModel.state
+    val context = LocalContext.current
+    LaunchedEffect(viewModel, context) {
+        viewModel.authResults.collect { result ->
+            when(result) {
+                is AuthResult.Authorized -> {
+                    navigator.navigate(SecretScreenDestination) {
+                        popUpTo(AuthScreenDestination.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+                is AuthResult.Unauthorized -> {
+                    Toast.makeText(
+                        context,
+                        "You're not authorized",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                is AuthResult.UnknownError -> {
+                    Toast.makeText(
+                        context,
+                        "An unknown error occurred",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = state.signUpUsername,
+            onValueChange = {
+                viewModel.onEvent(AuthUiEvent.SignUpUsernameChanged(it))
+            },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(text = "Username")
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = state.signUpPassword,
+            onValueChange = {
+                viewModel.onEvent(AuthUiEvent.SignUpPasswordChanged(it))
+            },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(text = "Password")
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                viewModel.onEvent(AuthUiEvent.SignUp)
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "Sign up")
+        }
+
+        Spacer(modifier = Modifier.height(64.dp))
+
+        TextField(
+            value = state.signInUsername,
+            onValueChange = {
+                viewModel.onEvent(AuthUiEvent.SignInUsernameChanged(it))
+            },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(text = "Username")
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        TextField(
+            value = state.signInPassword,
+            onValueChange = {
+                viewModel.onEvent(AuthUiEvent.SignInPasswordChanged(it))
+            },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text(text = "Password")
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                viewModel.onEvent(AuthUiEvent.SignIn)
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text(text = "Sign in")
+        }
+    }
+    if (state.isLoading) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/ingsw_24_25_dietiestates25/packagetest/MainViewModel2.kt
@@ -0,0 +1,88 @@
+package com.example.ingsw_24_25_dietiestates25.packagetest
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ingsw_24_25_dietiestates25.AuthState
+import com.example.ingsw_24_25_dietiestates25.AuthUiEvent
+import com.example.ingsw_24_25_dietiestates25.data.auth.AuthRepository
+import com.example.ingsw_24_25_dietiestates25.data.auth.AuthResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MainViewModel2 @Inject constructor(
+    private val repository: AuthRepository
+): ViewModel() {
+
+    var state by mutableStateOf(AuthState())
+
+    private val resultChannel = Channel<AuthResult<Unit>>()
+    val authResults = resultChannel.receiveAsFlow()
+
+    init {
+        authenticate()
+    }
+
+    fun onEvent(event: AuthUiEvent) {
+        when(event) {
+            is AuthUiEvent.SignInUsernameChanged -> {
+                state = state.copy(signInEmail = event.value)
+            }
+            is AuthUiEvent.SignInPasswordChanged -> {
+                state = state.copy(signInPassword = event.value)
+            }
+            is AuthUiEvent.SignIn -> {
+                signIn()
+            }
+            is AuthUiEvent.SignUpUsernameChanged -> {
+                state = state.copy(signUpEmail = event.value)
+            }
+            is AuthUiEvent.SignUpPasswordChanged -> {
+                state = state.copy(signUpPassword = event.value)
+            }
+            is AuthUiEvent.SignUp -> {
+                signUp()
+            }
+        }
+    }
+
+    private fun signUp() {
+        viewModelScope.launch {
+            state = state.copy(isLoading = true)
+            val result = repository.signUp(
+                email = state.signUpEmail,
+                password = state.signUpPassword
+            )
+            resultChannel.send(result)
+            state = state.copy(isLoading = false)
+        }
+    }
+
+    private fun signIn() {
+        viewModelScope.launch {
+            state = state.copy(isLoading = true)
+            val result = repository.signIn(
+                email = state.signInEmail,
+                password = state.signInPassword
+            )
+            resultChannel.send(result)
+            state = state.copy(isLoading = false)
+        }
+    }
+
+    private fun authenticate() {
+        viewModelScope.launch {
+            state = state.copy(isLoading = true)
+            val result = repository.authenticate()
+            resultChannel.send(result)
+            state = state.copy(isLoading = false)
+        }
+    }
+}
