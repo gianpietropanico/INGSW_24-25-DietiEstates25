44c9effdf08d8a6b2dab1fe54e9246e886f8053d
Gianpietro <panico.gianpietro@gmail.com>
2025-09-28 10:36:56 +0200

    ResultsScreen e lista case

--- app/src/main/java/com/example/ingsw_24_25_dietiestates25/data/repository/propertyListingRepo/PropertyListingRepository.kt
+++ app/src/main/java/com/example/ingsw_24_25_dietiestates25/data/repository/propertyListingRepo/PropertyListingRepository.kt
@@ -6,7 +6,7 @@ import com.example.ingsw_24_25_dietiestates25.data.model.result.ApiResult
 
 interface PropertyListingRepository {
 
-    suspend fun addPropertyListing(propertyListing: PropertyListing): ApiResult<Unit>
+    suspend fun addPropertyListing(propertyListing: PropertyListing): ApiResult<String>
     suspend fun getPropertiesListingByAgent(agentEmail: String): ApiResult<List<PropertyListing>>
     suspend fun getAllListings(): ApiResult<List<PropertyListing>>
     suspend fun getListingsWithinRadius(lat: Double, lon: Double, radius: Double): ApiResult<List<PropertyListing>>
@@ -15,4 +15,3 @@ interface PropertyListingRepository {
 
 }
 
-}
